fdf0373df99a83aa0c1f23291d9c41f7979ff946
DimaMinka <cdk@cdk.co.il>
2018-03-01 13:59:51 +0200

    feat--field--toggler frontend side ready

--- assets/js/components/field.js
+++ assets/js/components/field.js
@@ -8,7 +8,6 @@ import { compose, withHandlers, setStatic } from 'recompose';
 /**
  * The internal dependencies.
  */
-import Field from 'fields/components/field';
 import withStore from 'fields/decorators/with-store';
 import withSetup from 'fields/decorators/with-setup';
 
@@ -17,16 +16,20 @@ import withSetup from 'fields/decorators/with-setup';
  *
  * @param  {Object} props
  * @param  {Object} props.field
+ * @param  {Function} props.togglerHelper
+ * @param  {Function} props.addCollapsed
  * @return {React.Element}
  */
 export const TogglerField = ({
-	name,
-	field
+	field,
+  togglerHelper,
+  addCollapsed
 }) => {
-  return <Field field={field} showLabel={false}>
+  window.addEventListener('load', addCollapsed);
+  return <div onClick={togglerHelper} className={'collapsed carbon-toggler ' + field.id}>
     <h3>{field.label}</h3>
-  </Field>;
-}
+  </div>;
+};
 
 /**
  * Validate the props.
@@ -37,6 +40,8 @@ TogglerField.propTypes = {
   field: PropTypes.shape({
     label: PropTypes.string,
   }),
+  togglerHelper: PropTypes.func,
+  addCollapsed: PropTypes.func,
 };
 
 /**
@@ -54,9 +59,47 @@ export const enhance = compose(
 	 * Attach the setup hooks.
 	 */
 	withSetup(),
+
+  /**
+   * The handlers passed to the component.
+   */
+  withHandlers({
+
+    togglerHelper: ({ field }) => () => {
+      const target = document.getElementsByClassName(field.id);
+      const targetFields = document.getElementsByClassName('toggled-' + field.id);
+
+      target[0].classList.toggle('collapsed');
+
+      for (let i=0; i<targetFields.length; i++) {
+        targetFields[i].classList.toggle('uncollapsed');
+      }
+    },
+
+    addCollapsed: ({ field }) => () => {
+      const toggle = document.getElementsByClassName(field.id)[0];
+
+      toggle.classList.toggle('collapsed-enabled');
+
+      function nextElementSibling(element) {
+        if(element.nextSibling && !element.nextSibling.classList.contains('carbon-toggler')) {
+          element.nextSibling.classList.add('toggled-field');
+          element.nextSibling.classList.add('toggled-' + field.id);
+          nextElementSibling(element.nextSibling);
+        } else {
+          return false;
+        }
+
+      }
+
+      nextElementSibling(toggle);
+
+      return false;
+
+    }
+  })
 );
 
 export default setStatic('type', [
 	'toggler',
 ])(enhance(TogglerField));
-
